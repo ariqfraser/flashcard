50865f37ca33d38bb50e2ce8f4aa7002d65b378e
Ariq <ariqfraser@gmail.com>
2025-09-08 12:58:51 +0100

    feat: add Firebase emulator configuration and update app configuration for local development

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,6 +1,8 @@
 import {
     ApplicationConfig,
+    EnvironmentProviders,
     provideBrowserGlobalErrorListeners,
+    Provider,
     provideZoneChangeDetection,
 } from "@angular/core";
 import { provideRouter } from "@angular/router";
@@ -8,25 +10,35 @@ import { provideRouter } from "@angular/router";
 import { routes } from "./app.routes";
 import { initializeApp, provideFirebaseApp } from "@angular/fire/app";
 import { environment } from "../environments/environment";
-import { getAuth, provideAuth } from "@angular/fire/auth";
+import { connectAuthEmulator, getAuth, provideAuth } from "@angular/fire/auth";
 import {
     getAnalytics,
     provideAnalytics,
     ScreenTrackingService,
     UserTrackingService,
 } from "@angular/fire/analytics";
-import { getFirestore, provideFirestore } from "@angular/fire/firestore";
+import { connectFirestoreEmulator, getFirestore, provideFirestore } from "@angular/fire/firestore";
+
+const firebaseProviders: (Provider | EnvironmentProviders)[] = [
+    provideFirebaseApp(() => initializeApp(environment.firebase)),
+    provideAuth(() => {
+        if (environment.production) connectAuthEmulator(getAuth(), "http://localhost:9099");
+        return getAuth();
+    }),
+    provideAnalytics(() => getAnalytics()),
+    ScreenTrackingService,
+    UserTrackingService,
+    provideFirestore(() => {
+        if (environment.production) connectFirestoreEmulator(getFirestore(), "localhost", 8080);
+        return getFirestore();
+    }),
+];
 
 export const appConfig: ApplicationConfig = {
     providers: [
         provideBrowserGlobalErrorListeners(),
         provideZoneChangeDetection({ eventCoalescing: true }),
         provideRouter(routes),
-        provideFirebaseApp(() => initializeApp(environment.firebase)),
-        provideAuth(() => getAuth()),
-        provideAnalytics(() => getAnalytics()),
-        ScreenTrackingService,
-        UserTrackingService,
-        provideFirestore(() => getFirestore()),
+        ...firebaseProviders,
     ],
 };
