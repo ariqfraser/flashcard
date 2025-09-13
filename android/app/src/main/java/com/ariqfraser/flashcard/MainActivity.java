6fa05342e4de404a4a669bf5b14b763fcd436ad7
Ariq Fraser <ariq.fraser1@computershare.co.uk>
2025-09-14 00:15:58 +0100

    chore: update dependencies to include Capacitor packages

---
+++ android/app/src/main/java/com/ariqfraser/flashcard/MainActivity.java
@@ -0,0 +1,5 @@
+package com.ariqfraser.flashcard;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from "@capacitor/cli";
+
+const config: CapacitorConfig = {
+    appId: "com.ariqfraser.flashcard",
+    appName: "flashcard",
+    webDir: "dist/flashcard/browser",
+};
+
+export default config;
