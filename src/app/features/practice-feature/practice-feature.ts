33178d0ae40c3f3d410851dc9d0b2685aa173d98
Ariq Fraser <ariq.fraser1@computershare.co.uk>
2025-09-14 12:23:33 +0100

    feat: update practice feature layout with styled action buttons and include Icon component

--- src/app/features/practice-feature/practice-feature.ts
+++ src/app/features/practice-feature/practice-feature.ts
@@ -1,11 +1,12 @@
 import { Component, inject, signal } from "@angular/core";
 import { Router } from "@angular/router";
+import { Icon } from "@shared/ui/icon/icon";
 import { PageHeader } from "@shared/ui/page-header/page-header";
 import { TrainingCard } from "@shared/ui/training-card/training-card";
 
 @Component({
     selector: "app-practice-feature",
-    imports: [PageHeader, TrainingCard],
+    imports: [PageHeader, TrainingCard, Icon],
     templateUrl: "./practice-feature.html",
     styleUrl: "./practice-feature.scss",
 })
