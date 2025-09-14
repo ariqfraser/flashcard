d79b45eba6501f7a526eaeb9e22a7045328c4e84
Ariq Fraser <ariq.fraser1@computershare.co.uk>
2025-09-14 11:36:47 +0100

    test: enhance SafeInnerHtml spec to use TestBed for instance creation

--- src/app/shared/directives/safe-inner-html.spec.ts
+++ src/app/shared/directives/safe-inner-html.spec.ts
@@ -1,8 +1,32 @@
+import { Component } from "@angular/core";
+import { TestBed } from "@angular/core/testing";
+import { By } from "@angular/platform-browser";
 import { SafeInnerHtml } from "./safe-inner-html";
 
+@Component({
+    standalone: true,
+    imports: [SafeInnerHtml],
+    template: `<div [safeInnerHtml]="html"></div>`,
+})
+class HostComponent {
+    html = "<span>hi</span>";
+}
+
 describe("SafeInnerHtml", () => {
-    it("should create an instance", () => {
-        const directive = new SafeInnerHtml();
-        expect(directive).toBeTruthy();
+    beforeEach(() =>
+        TestBed.configureTestingModule({
+            imports: [HostComponent, SafeInnerHtml],
+        }),
+    );
+
+    it("should create an instance via TestBed", () => {
+        const fixture = TestBed.createComponent(HostComponent);
+        fixture.detectChanges();
+
+        const debug = fixture.debugElement.query(By.directive(SafeInnerHtml));
+        expect(debug).toBeTruthy();
+
+        const directiveInstance = debug.injector.get(SafeInnerHtml);
+        expect(directiveInstance).toBeTruthy();
     });
 });
